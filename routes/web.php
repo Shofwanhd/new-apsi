762c57440ff979bf0a34ef26e8a48d830649b2d7
Shofwan Hadi <haddishofwan12@gmail.com>
2024-01-06 01:22:34 +0700

    add dashboard

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\DashboarduserController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
@@ -28,8 +29,12 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::middleware('checkRole:user')->group(function () {
+    // Route::get('/dashboard', [DashboarduserController::class, 'index'])->name('index');
+});
+
 
 // ->middleware(['checkRole:owner,admin,user,member']);
 
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
