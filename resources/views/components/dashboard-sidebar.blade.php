f58b9f19b2dd37053fc9efe88e40eaffbc05f28d
Shofwan Hadi <haddishofwan12@gmail.com>
2024-01-06 10:02:00 +0700

    update owner

--- resources/views/components/dashboard-sidebar.blade.php
+++ resources/views/components/dashboard-sidebar.blade.php
@@ -66,7 +66,7 @@
         <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
             <ul class="space-y-2 font-medium">
                 <li>
-                    <a href="dashboard"
+                    <a href="/dashboard"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
@@ -151,7 +151,7 @@
         <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
             <ul class="space-y-2 font-medium">
                 <li>
-                    <a href="#"
+                    <a href="/dashboard"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
@@ -165,7 +165,7 @@
                     </a>
                 </li>
                 <li>
-                    <a href="#"
+                    <a href="/data-transaksi"
                         class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                         <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                             aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
@@ -217,7 +217,7 @@
                     </a>
                 </li>
                 <li> --}}
-                <a href="#"
+                <a href="/data-pengguna"
                     class="flex items-center p-2 text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700 group">
                     <svg class="flex-shrink-0 w-5 h-5 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
                         aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -64,6 +64,85 @@
             object-fit: cover;
         }
     </style>
+    <style>
+        .list {
+            font-family: sans-serif;
+        }
+
+        td {
+            padding: 10px;
+            border: solid 1px #eee;
+        }
+
+        input {
+            border: solid 1px #ccc;
+            border-radius: 5px;
+            padding: 7px 14px;
+            margin-bottom: 10px
+        }
+
+        input:focus {
+            outline: none;
+            border-color: #aaa;
+        }
+
+        .sort {
+            padding: 8px 30px;
+            border-radius: 6px;
+            border: none;
+            display: inline-block;
+            color: #fff;
+            text-decoration: none;
+            background-color: #28a8e0;
+            height: 30px;
+        }
+
+        .sort:hover {
+            text-decoration: none;
+            background-color: #1b8aba;
+        }
+
+        .sort:focus {
+            outline: none;
+        }
+
+        .sort:after {
+            display: inline-block;
+            width: 0;
+            height: 0;
+            border-left: 5px solid transparent;
+            border-right: 5px solid transparent;
+            border-bottom: 5px solid transparent;
+            content: "";
+            position: relative;
+            top: -10px;
+            right: -5px;
+        }
+
+        .sort.asc:after {
+            width: 0;
+            height: 0;
+            border-left: 5px solid transparent;
+            border-right: 5px solid transparent;
+            border-top: 5px solid #fff;
+            content: "";
+            position: relative;
+            top: 4px;
+            right: -5px;
+        }
+
+        .sort.desc:after {
+            width: 0;
+            height: 0;
+            border-left: 5px solid transparent;
+            border-right: 5px solid transparent;
+            border-bottom: 5px solid #fff;
+            content: "";
+            position: relative;
+            top: -4px;
+            right: -5px;
+        }
+    </style>
 </head>
 
 <body class="font-sans antialiased">
@@ -86,6 +165,13 @@
     </div>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/flowbite.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.2.1/datepicker.min.js"></script>
+    <script>
+        var options = {
+            valueNames: ['name', 'email', 'domisili', 'umur', 'role']
+        };
+
+        var userList = new List('users', options);
+    </script>
 
 </body>
 
